b812eab62d95d83ab519aaeff29263192ab5e261
taiht <ldcc.taiho@lotte.vn>
2025-06-11 10:58:49 +0700

    remove map

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -41,10 +41,10 @@ export default function Home() {
         <MapWrapper />
       </div> */}
 
-      <div className="p-4 max-w-4xl mx-auto">
+      {/* <div className="p-4 max-w-4xl mx-auto">
         <h1 className="text-xl font-bold mb-4">Bản đồ Việt Nam</h1>
         <VietNamMapSVG isShowProvinceList={true} />
-      </div>
+      </div> */}
 
       {/* Latest Properties Section */}
       <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
